733e166869b1d574a770f65101466d75edba0815
AlbertB93 <bercik93@interia.eu>
2023-09-28 14:56:25 +0200

    Added: - sass, - Recipe component

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,8 +2,7 @@ import React from "react";
 import "./Content.css";
 import { dishes } from "../data/dishes.js";
 import Day from "./Day";
-
-
+import Recipe from "./Recipe";
 
 class Content extends React.Component {
   /*  const dishHandler = (i) => props.calculateDay(props.dishes[i].id); */
@@ -14,6 +13,7 @@ class Content extends React.Component {
     fats: 0,
     carbons: 0,
     proteins: 0,
+    isDisplay: false,
   };
 
   addMeal = (index) => {
@@ -25,55 +25,56 @@ class Content extends React.Component {
     this.setState({ proteins: dishes[index].proteins });
   };
 
-
   render() {
     return (
       <div id="content--container">
-      <div id="dishes">
-        {dishes?.map((dish, index) => {
-          return (
-            <div className="single--dish" key={index}>
-              <div className="single--dish__image">
-                <img
-                  src={require("../images/dish1.PNG")}
-                  alt="dish1"
-                  className="image--file"
-                />
+        <div id="dishes">
+          {dishes?.map((dish, index) => {
+            return (
+              <div className="single--dish" key={index}>
+                <div className="single--dish__image">
+                  <img
+                    src={require("../images/dish1.PNG")}
+                    alt="dish1"
+                    className="image--file"
+                  />
+                </div>
+                <h3>{dish.title}</h3>
+                <div className="single--dish__values">
+                  <ul>
+                    <li>Wartość energetyczna: {dish.kcal} kcal.</li>
+                    <li>Tłuszcze: {dish.fats} g. </li>
+                    <li>Węglowodany: {dish.carbons} g.</li>
+                    <li>Białko: {dish.proteins} g.</li>
+                  </ul>
+                </div>
+                <div className="single--dish__buttons">
+                  <button className="showRecipe_btn" onClick={this.addToDay}>
+                    Pokaż przepis
+                  </button>
+                  <button
+                    className="showRecipe_btn"
+                    onClick={() => this.addMeal(index)}
+                  >
+                    Dodaj do jadłospisu
+                  </button>
+                </div>
               </div>
-              <h3>{dish.title}</h3>
-              <div className="single--dish__values">
-                <ul>
-                  <li>Wartość energetyczna: {dish.kcal} kcal.</li>
-                  <li>Tłuszcze: {dish.fats} g. </li>
-                  <li>Węglowodany: {dish.carbons} g.</li>
-                  <li>Białko: {dish.proteins} g.</li>
-                </ul>
-              </div>
-              <div className="single--dish__buttons">
-                <button className="showRecipe_btn" onClick={this.addToDay}>Pokaż przepis</button>
-                <button
-                  className="showRecipe_btn"
-                  onClick={() => this.addMeal(index)}
-                >
-                  Dodaj do jadłospisu
-                </button>
-              </div>
-            </div>
-          );
-        })}
+            );
+          })}
         </div>
         <Day posilek={this.state} funkcja={this.addMeal} />
-        <div id="testowy">
-                  <h2>Aktualnie wybrany posiłek: </h2>
-        <div>
-          <p className="test"> Nazwa: {this.state.title}</p>
-          <p className="test"> Kcal: {this.state.kcal}</p>
-          <p className="test">F: {this.state.fats}</p>
-          <p className="test">W: {this.state.carbons}</p>
-          <p className="test">B: {this.state.proteins}</p>
-        </div>
-        </div>
-
+        {/*         <div id="testowy">
+          <h2>Aktualnie wybrany posiłek: </h2>
+          <div>
+            <p className="test"> Nazwa: {this.state.title}</p>
+            <p className="test"> Kcal: {this.state.kcal}</p>
+            <p className="test">F: {this.state.fats}</p>
+            <p className="test">W: {this.state.carbons}</p>
+            <p className="test">B: {this.state.proteins}</p>
+          </div>
+        </div> */}
+        <Recipe posilek={this.state} />
       </div>
     );
   }
---
+++ src/components/Recipe.js
@@ -0,0 +1,58 @@
+import "./Recipe.scss";
+
+const Recipe = (props) => {
+  return (
+    <div id="recipe--container">
+      <div className="recipe--container__title">
+        <h2>{props.posilek.title}</h2>
+        <button className="btn__close">Zamknij </button>
+      </div>
+      <div className="recipe--container__imageAndIgredients">
+        <div className="image">
+          <img
+            src={require("../images/dish1.PNG")}
+            alt="dish1"
+            className="image--file"
+          />
+        </div>
+        <div className="ingredients">
+          <ul>
+            <li>Składnik 1 z innego pliku</li>
+            <li>Składnik 1 z innego pliku</li>
+            <li>Składnik 1 z innego pliku</li>
+            <li>Składnik 1 z innego pliku</li>
+            <li>Składnik 1 z innego pliku</li>
+            <li>Składnik 1 z innego pliku</li>
+            <li>Składnik 1 z innego pliku</li>
+            <li>Składnik 1 z innego pliku</li>
+          </ul>
+        </div>
+      </div>
+      <div className="recipe--container__description">
+        <p>Tekst pobrany</p>
+        <p>
+          {" "}
+          Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w
+          przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez
+          nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć
+          wieków później zaczął być używany przemyśle elektronicznym, pozostając
+          praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz
+          z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a
+          ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem
+          przeznaczonym do realizacji druków na komputerach osobistych, jak
+          Aldus PageMakerLorem Ipsum jest tekstem stosowanym jako przykładowy
+          wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w
+          XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej
+          książki. Pięć wieków później zaczął być używany przemyśle
+          elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował
+          się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu,
+          zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne
+          wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków
+          na komputerach osobistych, jak Aldus PageMaker{" "}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Recipe;
